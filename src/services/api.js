55adab7015e5fdbeb8995658f3d3f63c3186b3c4
uday7223 <uday.devnur223@gmail.com>
2025-07-03 17:32:41 +0530

    updated deployement url

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://fastapi-todo-backend-production.up.railway.app",
+  baseURL: "https://fastapi-todo-backend-production.up.railway.app",
 
   // for local testing
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
   },
